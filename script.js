2bcc30b01c30163888107c4bd67cde57c3d9c529
devon96 <kdryja@gmail.com>
2019-10-08 20:34:39 +0100

    Pass name of the beacon as GET param. Also modify Puck.JS code a bit, so it will only scan for BT devices matching the name. Finally, don't initiate connection at all if no name provided

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 function receiveData() {
-	Puck.eval("BTN.read()",function(x) { sendData(x) })
+  var queryDict = {};
+  location.search.substr(1).split("&").forEach(function(
+      item) { queryDict[item.split("=")[0]] = item.split("=")[1] });
+  var name = queryDict["n"];
+  if (name) {
+    Puck.eval("BTN.read()", name, function(x) { sendData(x) })
+  } else {
+    console.log("name not provided!");
+  }
 }
 function sendData(data) {
   $.ajax({
-    url: "https://httpbin.org/post",
-    type: 'POST',
-    data: data,
-    success: function(d, s) {
-      console.log(s);
-    }
+    url : "https://httpbin.org/post",
+    type : 'POST',
+    data : data,
+    success : function(d, s) { console.log(s); }
   });
 }
