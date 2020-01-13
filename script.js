473692d54318eb2c78f44f68f612a3f4be8dc11a
devon96 <kdryja@gmail.com>
2020-01-13 19:00:20 +0000

    add delivery to blockchain

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function getPuckData(name) {
   return new Promise((resolve, reject) => {
-    Puck.eval("getAll()", name, function(data) {
+    Puck.eval("getAll()", name, function (data) {
       puckData = JSON.parse(data);
       sendData(data);
+      addTranscation(data);
       resolve(puckData);
     });
   });
@@ -10,8 +11,8 @@ function getPuckData(name) {
 
 async function receiveData() {
   var queryDict = {};
-  location.search.substr(1).split("&").forEach(function(
-      item) { queryDict[item.split("=")[0]] = item.split("=")[1] });
+  location.search.substr(1).split("&").forEach(function (
+    item) {queryDict[item.split("=")[0]] = item.split("=")[1]});
   var name = queryDict["n"];
   var puckData;
   if (name) {
@@ -29,9 +30,28 @@ async function receiveData() {
 }
 function sendData(data) {
   $.ajax({
-    url : "https://dryja.dev/webapp",
-    type : 'POST',
-    data : data,
-    success : function(d, s) { console.log(s); }
+    url: "https://dryja.dev/webapp",
+    type: 'POST',
+    data: data,
+    success: function (d, s) {console.log(s);}
+  });
+}
+function addTranscation(report) {
+  let newEntity = "MestonDelivery";
+  let delivery = "mySymbol";
+  $.ajax({
+    url: "http://trustlens.abdn.ac.uk/api/Delivery",
+    type: 'POST',
+    beforeSend: function (xhr) {
+      // TODO(kdryja): DO NOT STORE AS PLAINTEXT!!!!!!
+      xhr.setRequestHeader('Authorization', 'Basic ' + btoa('kdryja:aberdeen2020'))
+    },
+    data: {
+      "commodity": delivery,
+      "newOwner": newEntity,
+      "status": "delivered",
+      "complianceReport": JSON.stringify(report)
+    },
+    success: function (d, s) {console.log(s);}
   });
 }
