ceb9d78ece582e871d3f892f1a30b4e890504f27
devon96 <kdryja@gmail.com>
2020-02-02 12:48:06 +0000

    Add deliveryID key

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,9 +1,17 @@
 let complianceReport = {"hello": "world"};
 
+function uuidv4() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
 function getPuckData(name) {
   return new Promise((resolve, reject) => {
     Puck.eval("getAll()", name, function (data) {
       puckData = JSON.parse(data);
+      data["deliveryID"] = uuidv4();
       sendData(data);
       resolve(puckData);
     });
